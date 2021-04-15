cae92e044de7724604a000d538e6bb46ffab9125
adelakuntobi <adelakuntobiloba1@gmail.com>
2021-04-15 21:56:45 +0100

    Fixed minor erros

---
+++ js/complete.js
@@ -0,0 +1,12 @@
+const Btn = document.querySelector("#btn"),
+modal = document.querySelector("#modal-tension"),
+abortTest = document.querySelector("#abort-test")
+
+const openModal = () => {
+  modal.style.display = "flex"
+}
+
+
+const closeModal = () => {
+  modal.style.display = "none"
+  }
